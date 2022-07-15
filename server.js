edb545d210adf90305864a6442d8502934c58f23
mottybar <mottybar20@gmail.com>
2022-07-15 17:36:19 +0300

    fix served files folder name, gitignore build folder

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const path = require("path");
 const express = require("express");
 const app = express();
 const cors = require("cors");
@@ -5,16 +6,18 @@ require("dotenv").config({ path: "./config.env" });
 const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
+
+const publicPath = path.join(__dirname, "client", "build");
+app.use(express.static(publicPath));
+
 app.use(require("./routes/record"));
 // get driver connection
 const dbo = require("./db/conn");
- 
+
 app.listen(port, () => {
   // perform a database connection when server starts
   dbo.connectToServer(function (err) {
     if (err) console.error(err);
- 
   });
   console.log(`Server is running on port: ${port}`);
 });
-
