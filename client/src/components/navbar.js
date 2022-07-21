69a0ccd1885bc0cb1ec52ecb7356ce2d17a7c480
mottybar <mottybar20@gmail.com>
2022-07-21 21:42:16 +0300

    change picture

--- client/src/components/navbar.js
+++ client/src/components/navbar.js
@@ -33,7 +33,7 @@ export default function Navbar() {
           <ul className="navbar-nav ml-auto">
             <li className="nav-item">
               <NavLink className="nav-link" to="/create">
-                <h1>Create Record</h1>
+                Create Record
               </NavLink>
             </li>
           </ul>
